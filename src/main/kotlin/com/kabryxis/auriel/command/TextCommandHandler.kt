c8113b60112516dbc3f36edb679b0cb6b7037d2b
Warren1001 <kabryxis@gmail.com>
2021-04-30 20:56:05 -0500

    finally fixed, thank the lord

--- src/main/kotlin/com/kabryxis/auriel/command/TextCommandHandler.kt
+++ src/main/kotlin/com/kabryxis/auriel/command/TextCommandHandler.kt
@@ -10,7 +10,6 @@ class TextCommandHandler(val name: String, val template: String) : CommandHandle
 		message.channel.flatMap { it.createMessage(format(message, arg)) }.subscribe()
 	}
 	
-	// a
 	private fun format(message: Message, arg: String) : String {
 		var msg = template;
 		if (message.author.isPresent) msg = msg.replace("{user}", message.author.get().mention)
